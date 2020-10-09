b6f652b58990c8dd951adf70dbfc205126f81ee7
pyzhovartyom <pyzhovartyom@gmail.com>
2020-10-09 21:40:54 +0300

    feat: add reverse func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = `${n}`;
+    if (result[0] === "-") {
+        result = result.slice(1);
+    }
+    console.log(result)
+    result = `${result}`.split('').reverse().join('');
+    return Number(result)
 }
